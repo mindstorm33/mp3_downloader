40cf6b10cdb3022ec0fd17dc77f97b0720dba34a
mindstorm33 <72254904+mindstorm33@users.noreply.github.com>
2020-11-30 22:51:20 +0530

    Add files via upload

---
+++ mp3.py
@@ -0,0 +1,41 @@
+#!python3
+
+import sys, pyperclip, time
+from selenium import webdriver 
+
+browser = webdriver.Chrome()
+search = ' '.join(sys.argv[1:])
+# search = 'Anderson .Paak - Trippy'
+
+browser.get('https://youtube.com')
+search_box = browser.find_element_by_tag_name('input')
+search_box.send_keys(search)
+search_box.submit()
+
+time.sleep(3)
+vid_link = browser.find_element_by_id('video-title').get_attribute('href')
+
+browser.get('https://y2mate.com')
+search_box = browser.find_element_by_tag_name('input')
+search_box.send_keys(vid_link)
+search_box.submit()
+
+time.sleep(3)
+browser.find_element_by_link_text('mp3').click()
+time.sleep(3)
+browser.find_element_by_link_text('Download').click()
+time.sleep(3)
+try:
+    browser.find_element_by_link_text('Download .mp3').click()
+except:
+    time.sleep(5)
+    browser.find_element_by_link_text('Download .mp3').click()
+    
+
+
+
+
+
+
+
+
